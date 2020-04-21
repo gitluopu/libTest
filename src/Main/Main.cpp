c71485d3a50c76fbcb77c61d813251e3daf12b9d
luopu <951634379@qq.com>
2020-04-21 16:53:34 +0800

    优化

--- src/Main/Main.cpp
+++ src/Main/Main.cpp
@@ -3,10 +3,13 @@
 #include "AceWrap/Time.h"
 using namespace std;
 #define  PATH "C:\\Users\\95163\\Desktop\\test.json"
+
 int main(int argc, char *argv[])
 {
-	AceTime at;
-	LOG_INFO("%s\n",at.TimeStampMs().c_str());
+#ifdef _WIN32
 	system("pause");
+#else
+	system("read");
+#endif
 	return 0;
 }
